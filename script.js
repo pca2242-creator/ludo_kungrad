3105eb0db7eed886809aa9cf44ada81b0b212394
pca2242-creator <pca2242@gmail.com>
2026-09-15 00:31:15 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let currentTurn = 'red';
 
 function playTurn() {
+    // 1 dan 6 gacha tasodifiy son chiqarish
     const diceValue = Math.floor(Math.random() * 6) + 1;
     document.getElementById('dice-view').innerText = diceValue;
     const turnText = document.getElementById('turn-text');
 
     if (currentTurn === 'red') {
         currentTurn = 'blue';
-        turnText.innerHTML = Navbat: <span style="color: #4da6ff; font-weight: bold;">Ko'k</span>;
+        turnText.innerHTML = Navbat: <span style="color: #3498db; font-weight: bold;">Ko'k</span>;
     } else {
         currentTurn = 'red';
-        turnText.innerHTML = Navbat: <span style="color: #ff4d4d; font-weight: bold;">Qizil</span>;
+        turnText.innerHTML = Navbat: <span style="color: #e74c3c; font-weight: bold;">Qizil</span>;
     }
 }
